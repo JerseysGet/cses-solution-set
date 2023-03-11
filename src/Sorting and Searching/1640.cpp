185b4b5f0a6d722139c3d34dab6ba37e63129387
JerseysGet <dev.faisal2004@gmail.com>
2023-03-11 13:52:21 +0530

    Added solution to problem 1640

--- src/Sorting and Searching/1640.cpp
+++ src/Sorting and Searching/1640.cpp
@@ -5,34 +5,24 @@ using namespace std;
 
 int32_t main() {
     int n, x; cin >> n >> x;
-    multiset<int> arr;
-    unordered_map<int, vector<int>> indexOf;
-    int alsoArr[n];
+    vector<pair<int, int>> arr;
     int k;
     for (int i = 0; i < n; i++) {
-        cin >> alsoArr[i];
-        arr.insert(alsoArr[i]);
-        indexOf[alsoArr[i]].push_back(i+1);
+        cin >> k;
+        arr.push_back({k, i+1});
     }
-
-    sort(alsoArr, alsoArr + n);
-
-    for (const auto& ele : arr) {
-        auto itr = arr.find(ele);
-
-        arr.erase(itr);
-        auto pos = arr.find(x - ele);
-        if (pos != arr.end()) {
-            if (ele == x - ele) {
-                cout << indexOf[ele][0] << " " << indexOf[ele][1] << "\n";
-            } else {
-                cout << indexOf[ele][0] << " " << indexOf[x - ele][0] << "\n";
-            }
+    sort(arr.begin(), arr.end());
+    int L = 0, R = arr.size()-1;
+    while (L < R) {
+        if (arr[L].first + arr[R].first > x) {
+            R--;
+        } else if (arr[L].first + arr[R].first < x) {
+            L++;
+        } else {
+            cout << arr[L].second << " " << arr[R].second << "\n";
             return 0;
         }
-        arr.insert(ele);
     }
 
     cout << "IMPOSSIBLE\n";
-    return 0;
 }
